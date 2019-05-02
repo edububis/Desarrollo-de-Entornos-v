14abdffedf715e295489052872a1ed82f60f40f2
edububis <50111077+edububis@users.noreply.github.com>
2019-05-02 19:23:43 +0200

    Create Pagos.java

---
+++ Pagos.java
@@ -0,0 +1,38 @@
+package examen;
+
+import java.io.*;
+
+public class Pagos {
+	
+	public String factura;
+	public int trimestre;
+	public int año;
+	public double importe;
+	
+	public Pagos() {
+		this.factura="";
+		this.trimestre=0;
+		this.año=0;
+		this.importe=0.0;
+	}
+	
+	public Pagos(String[] datos) {
+		this.factura=datos[1];
+		this.trimestre=Integer.parseInt(datos[2]);
+		this.año=Integer.parseInt(datos[3]);
+		this.importe=Double.parseDouble(datos[4]);
+		
+	}
+	
+	public String ToString() {
+	 return " Factura " + factura +  " perteneciente al trimestre " + trimestre + " del año " + año + " por importe de "+importe+" euros";
+	}
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		Pagos P=new Pagos();
+		P.ToString();
+
+	}
+
+}
